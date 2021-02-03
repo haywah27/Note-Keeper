010d748a8e30072c2f09c811136f10535db1a833
haywah27 <hayleywahlroos15@gmail.com>
2021-02-03 15:35:54 -0800

    rethinking project/ honed in requirements on server.js

--- server.js
+++ server.js
@@ -1,23 +1,21 @@
-var bodyParser = require("body-parser");
-var express = require("express");
-
-var path = require("path");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
  
-var app = express();
-var PORT = process.env.PORT || 8080;
+// initializing express server
+const app = express();
+const PORT = process.env.PORT || 8080;
  
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "Develop/public")));
 
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
- 
-// parse application/json
-app.use(bodyParser.json());
-
-
+// routes
 require("./Develop/routes/api-routes")(app);
 require("./Develop/routes/html-routes")(app);
 
+// start server on PORT
 app.listen(PORT, function(){
     console.log("App is listening on PORT: " + PORT);
 });
