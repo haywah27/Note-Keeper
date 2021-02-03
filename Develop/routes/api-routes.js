872fb77d8e231842e12505d52d05f5859a73be5f
haywah27 <hayleywahlroos15@gmail.com>
2021-02-02 17:45:17 -0800

    adding post method to append note to db array

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -4,4 +4,9 @@ module.exports = function (app) {
     app.get("/api/notes", function (req, res){
         res.json(noteData);
     });
+
+    app.post("/api/notes", function (req, res){
+        noteData.push(req.body);
+        res.json(true);
+    });
 }
