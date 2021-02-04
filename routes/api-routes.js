f38ba74a48a40d4e0e3344540bf1a9aae377d14e
haywah27 <hayleywahlroos15@gmail.com>
2021-02-03 16:57:19 -0800

    post function added on api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,37 @@
-var noteData = require("../db/db.json");
+
 const fs = require('fs');
 
 module.exports = function (app) {
     app.get("/api/notes", function (req, res){
-        fs.readFile('db/db.json', (err, data)=> {
+        fs.readFile('./db/db.json', (err, data) => {
             if (err) throw err;
-            notesData = JSON.parse(data)
+            notesData = JSON.parse(data);
             res.send(notesData);
-        })
+        });
     });
 
     app.post("/api/notes", function (req, res){
-       
-        notesData = JSON.parse(data)
-        noteData.push(req.body);
-        res.json(true);
+        const notesOB = req.body;
+
+        fs.readFile('./db/db.json', (err, data) => {
+            if (err) throw err;
+            notesData = JSON.parse(data);
+            notesData.push(notesOB);
+            let noteNum = 1;
+            notesData.forEach((note, index) => {
+                note.id = noteNum;
+                noteNum++;
+                return notesData;
+            });
+            // console.log(JSON.stringify(notesData));
+            
+            noteStringify = JSON.stringify(notesData);
+
+            fs.writeFile("./db/db.json", noteStringify, (err, data) => {
+                if (err) throw err;
+            });
+        });
+        res.send("Note taken.");
+
     });
 }
